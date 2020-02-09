3fda59dc42d79af2cd20c53e0b2919b8f28e61db
crudenas <c@c.com>
2020-02-09 19:33:47 +0100

    add math

--- main.go
+++ main.go
@@ -5,5 +5,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, VICTOR!")
+
+	var a int
+	b := 6 + 4
+	c := 20
+	answer := c - b
+	a = answer
+
+	fmt.Println("a är =", a)
 }
+
+// 6+4+a=20
+/*
+10*8*8+a=1000
+*/
